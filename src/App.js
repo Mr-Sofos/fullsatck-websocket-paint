be6c1985589e5e742effa874598f22f493c26503
Mokhmad Tazbiev <mokhmad.tazbiev@gmail.com>
2021-11-24 18:31:53 +0300

    add styles and icons for toolbar and also anoter things

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./styles/app.scss";
+import Toolbar from "./components/Toolbar";
+import SettingBar from "./components/SettingBar";
+import Canvas from "./components/Canvas";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Toolbar />
+      <SettingBar />
+      <Canvas />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SettingBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SettingBar = () => {
+  return <div className="settingBar"></div>;
+};
+
+export default SettingBar;
